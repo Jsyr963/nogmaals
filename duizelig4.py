7d369bc3d9badd7c6f0bd266a4ec9b9f1273b46a
Jsyr963 <89968301+Jsyr963@users.noreply.github.com>
2021-11-23 10:25:28 +0100

    Tweede commit!

---
+++ duizelig4.py
@@ -0,0 +1,24 @@
+Dag = input("Kies een dag van de week: ")
+a = 2
+while a < 3:
+    if Dag == "Maandag":
+        print("Maandag")
+        break
+    elif Dag == "Dinsdag":
+        print("Maandag", "Dinsdag")
+        break
+    elif Dag == "Woensdag":
+        print("Maandag", "Dinsdag", "Woensdag")
+        break
+    elif Dag == "Donderdag":
+        print("Maandag", "Dinsdag", "Woensdag", "Donderdag")
+        break
+    elif Dag == "Vrijdag":
+        print("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag")
+        break
+    elif Dag ==  "Zaterdag":
+        print("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag")
+        break
+    elif Dag == "Zondag":
+        print("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag")
+        break
---
+++ duizelig5.py
@@ -0,0 +1,6 @@
+invoer = ""
+i = 0
+while invoer != "quit":
+    invoer = input("voer een getal in: ")
+    i = i + 1
+    print ("U heeft", i, "keer gebruik gemaakt van de input!" )
